f9b99345f9d964add52fa5b67d5d859e02ef8010
Samir Antonio <samir.ma@gmail.com>
2022-10-09 18:21:06 +0100

    update whale

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 // Random user's address that happens to have a lot of DAI on Polygon Mainnet
-const DAI_WHALE = "0xD92B63D0E9F2CE9F77c32BfeB2C6fACd20989eB3";
+const DAI_WHALE = "0xdfd74e3752c187c4ba899756238c76cbeefa954b";
 
 // Mainnet Pool contract address
 const POOL_ADDRESS_PROVIDER = "0xa97684ead0e402dc232d5a977953df7ecbab3cdb";
